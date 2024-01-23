c954ed5d2afcbfc4ab6183d06c67cde1ee838a3b
sebastian461 <sebastiano-461@hotmail.com>
2024-01-23 16:33:30 -0500

    Añadiendo el calendario de react-big-calendar

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,9 +1,53 @@
+import { Calendar } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
+import { addHours } from "date-fns";
 import { Navbar } from "../components/Navbar";
+import { localizer, getMessages } from "../helpers";
+
+const events = [
+  {
+    title: "Cumpleaños",
+    notes: "Hay que comprar el pastel",
+    start: new Date(),
+    end: addHours(new Date(), 2),
+    bgColor: "#fafafa",
+    user: {
+      _id: "123",
+      name: "Sebastián",
+    },
+  },
+];
 
 export const CalendarPage = () => {
+  const eventStyleGetter = (event, start, end, isSelected) => {
+    //console.log({ event, start, end, isSelected });
+
+    const style = {
+      backgroundColor: "#347CF7",
+      borderRadius: "0px",
+      opacity: 0.8,
+      color: "white",
+    };
+
+    return {
+      style,
+    };
+  };
+
   return (
     <>
       <Navbar />
+      <Calendar
+        culture="es"
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: "calc(100vh - 80px)" }}
+        messages={getMessages()}
+        eventPropGetter={eventStyleGetter}
+      />
     </>
   );
 };

66ee9bebb404dd1850141adc14723bf59c8fd8c3
sebastian461 <sebastiano-461@hotmail.com>
2024-01-25 17:59:50 -0500

    Instalación y configuración de redux

--- src/CalendarApp.jsx
+++ src/CalendarApp.jsx
@@ -1,5 +1,11 @@
+import { Provider } from "react-redux";
 import { AppRouter } from "./router";
+import { store } from "./store/store";
 
 export const CalendarApp = () => {
-  return <AppRouter />;
+  return (
+    <Provider store={store}>
+      <AppRouter />
+    </Provider>
+  );
 };

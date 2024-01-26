59786152fb9137de23d5e4c139eb6f6246a374e6
sebastian461 <sebastiano-461@hotmail.com>
2024-01-25 20:40:08 -0500

    Corrección serializableCheck

--- src/store/store.js
+++ src/store/store.js
@@ -7,4 +7,8 @@ export const store = configureStore({
     calendar: calendarSlice.reducer,
     ui: uiSlice.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
